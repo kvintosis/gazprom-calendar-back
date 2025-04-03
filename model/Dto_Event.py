afe232371192a53d1baa69799c56eac96f3b7683
q297 <ilja.krasnokutskij@yandex.ru>
2025-04-04 00:19:15 +0700

    fix last request

--- model/Dto_Event.py
+++ model/Dto_Event.py
@@ -1,19 +1,7 @@
-from enum import Enum
-
 from pydantic import BaseModel
 from datetime import datetime
-class EventType(str, Enum):
-    meeting = "meeting"
-    birthday = "birthday"
-    other = "other"
 
-class Dto_Event(BaseModel):
-    title: str
-    description: str
-    start_time: datetime
-    end_time: datetime
-    type: EventType
-    organizer_id: int
+
 class EventCreate(BaseModel):
     title: str
     description: str | None = None
