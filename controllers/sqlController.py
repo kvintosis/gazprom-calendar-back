b9102cd514c44cd856571bfe01e5351aca376bfb
Genr0y <130198024+Genr0y@users.noreply.github.com>
2025-04-03 12:11:55 +0700

    Delete controllers/sqlController.py

--- controllers/sqlController.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.automap import automap_base
-engine = create_engine('sqlite:///../databases/employees.sqlite')
-Base = automap_base()
-Base.prepare(autoload_with=engine)
-
-# Получение класса для существующей таблицы
-ExistingTable = Base.classes.existing_table_name
-print(ExistingTable)
